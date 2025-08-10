25a7251d4f0ce2e072efa1f4a5b8ef0efd4c25c2
Ajay Gaud <gaudajay399@gmail.com>
2025-08-10 16:37:33 +0530

    Added projects row

--- src/app/components/home/components/dashboard/dashboard.component.ts
+++ src/app/components/home/components/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from '../../../authentication/models/project';
 import { Constants } from '../../../../../constants';
+import { SharedModule } from '../../../../../shared-module';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
